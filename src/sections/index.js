cce461601e01216f78d473cf8a5f1daed7d08847
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-04-06 22:08:33 +0100

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Footer from './sections/Footer'
+import SpecialOffer from './sections/SpecialOffer'
+import SuperQuality from './sections/SuperQuality'
+import Services from './sections/Services'
+import CustomerReviews from './sections/CustomerReviews'
+import Subscribe from './Subscribe'
+import PopularProducts from './Popular'
+
+export {
+    Hero,
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Subscribe,
+    Footer
+}
