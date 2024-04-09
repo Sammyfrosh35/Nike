0dbda0818f98aa1c213bc37881466ada40ae82d8
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-04-09 18:23:30 +0100

    new changes

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -19,6 +19,11 @@ const Nav = () => {
             </li>
           ))}
         </ul>
+        <div className="hidden max-lg:block">
+            <img src={hamburger} alt="hamburger"
+            width={25}
+            height={25} />
+        </div>
       </nav>
     </header>
   );
