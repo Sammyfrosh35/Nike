ba4d4c4c64c3c37573e489248fb0903a60b265c6
Sammyfrosh35 <badejosamuel9@gmail.com>
2024-04-12 07:28:04 +0100

    new chnages

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,17 +1,19 @@
 import React from 'react'
 
-const Button = ({label, iconURL,label,
+const Button = ({label, iconURL,
   backgroundColor,
   borderColor,
-  textColor}) => {
+  textColor, fullWidth }) => {
   return (
    <button className={`flex justify-center items-center gap-2 
    px-7 py-4 border font-montserrat text-lg 
    leading-none
-   ${backgroundColor}
-   
-   bg-coral-red
-   rounded-full text-white `}>
+   ${
+    backgroundColor 
+    ? `${backgroundColor} ${textColor} ${borderColor}` 
+   : "bg-coral-red text-white  border-coral-red"} 
+   rounded-full ${fullWidth && 'w-full'}"}`}
+   >
     {label}
 
     {iconURL && <img src={iconURL} alt="arrow right icon" 
--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,7 +1,33 @@
+import ReviewCard from "../Components/ReviewCard"
+import { reviews } from "../Constants"
 
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+   <section className="max-container ">
+<h3 className="font-palaquin text-center 
+text-4xl font-bold"> What Our  
+   <span className="text-coral-red"> Customers </span>
+    Say?
+</h3>
+<p className="info-text mt-4 m-auto max-w-lg text-center">
+   Hear geniune stories and feedbacks from our satisfied 
+   customers about their exceptional experiences with us.
+   </p>
+
+   <div className="mt-24 flex flex-1 justify-evenly
+    items-center max-lg:flex-col gap-14">
+      {reviews.map((review)=> (
+        <ReviewCard 
+        key={review.customerName}
+        imgURL={review.imgURL}
+        customerName={review.customerName}
+        rating={review.rating}
+        feedback={review.feedback}
+        />
+      ))}
+
+   </div>
+   </section>
   )
 }
 
