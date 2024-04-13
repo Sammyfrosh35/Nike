965cabc5e81916ba6770bc6ea1c525d5077e0f9a
Sammyfrosh35 <133043517+Sammyfrosh35@users.noreply.github.com>
2024-04-13 22:30:43 +0100

    Update Footer.jsx

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -13,8 +13,8 @@ const Footer = () => {
       height={46} />
     </a>
     <p className="mt-6 text-base leading-7 font-montserrat
-     text-white-400 sm:max-w-sm">Get shoes ready for the new term at 
-     your nearest Nike store. 
+     text-white-400 sm:max-w-sm">Get shoes ready for the new section at 
+     the nearest Nike store. 
      Get rewards
      </p>
      <div className="flex items-center gap-5 mt-8 cursor-pointer">
